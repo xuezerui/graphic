18d6e17e94b29adab74125a1aa89bce2081e089c
xuezerui <2218539769@qq.com>
2025-07-18 17:23:43 +0800

    基础框架

---
+++ src/test/java/com/example/graphic/GraphicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
